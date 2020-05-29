62ac1c37da46944275c30a78c66b2f870eccd14a
abc <sr.samanraja@gmail.com>
2020-05-29 12:44:24 +0530

    creating routes responses in express

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-//jshint version:6
-require('dotenv').config();
+
 const express = require('express');
 const dotenv = require('dotenv');
 
 //Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res
+    .status(200)
+    .json({ success: true, msg: 'Show bootcamp ' + req.params.id });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create all bootcamps' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: 'update all bootcamps with id ' + req.params.id  });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: 'delete all bootcamps with id ' + req.params.id  });
+});
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000,function(){
-    console.log("your server is running on port 5000");
-  });
+app.listen(
+    PORT,
+    console.log('Server running in ' + process.env.NODE_ENV + ' mode on port ' + PORT)
+)
